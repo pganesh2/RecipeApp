f49b7bd44d0023ffaed35fba891e71a51647f914
Pranav Ganesh <pganesh2@illinois.edu>
2018-05-02 05:41:52 -0500

    fuck yeah 5:41

--- app/src/main/java/edu/illinois/cs/cs125/recipeapp/RecipeList.java
+++ app/src/main/java/edu/illinois/cs/cs125/recipeapp/RecipeList.java
@@ -3,14 +3,20 @@ package edu.illinois.cs.cs125.recipeapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class RecipeList extends AppCompatActivity {
+    public static final String TAG1 = "RecipeList";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_list);
+        Intent i = getIntent();
+        String testRecipe = i.getExtras().getString("key");
+        Log.i(TAG1, "test recipe is here!");
+        Log.i(TAG1, testRecipe);
         goToRecipe();
 
     }
